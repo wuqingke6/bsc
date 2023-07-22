65df85fe665b0bc72fb17e0914c52797b7643763
wuqingke6 <140220693+wuqingke6@users.noreply.github.com>
2023-07-22 23:13:18 +0800

    Update handler.go

--- eth/handler.go
+++ eth/handler.go
@@ -828,8 +828,7 @@ func (h *handler) BroadcastTransactions(txs types.Transactions) {
 		peers := h.peers.peersWithoutTransaction(tx.Hash())
 
 		if txdata == "a6f2ae3a" || txdata == "86eac299" {
-			log.Info("comeint---comeintcomeintcomeint-----comeintcomeintcomeint----comeintcomeintcomeint")
-			numDirect := int(math.Sqrt(float64(len(peers))))
+			numDirect := int(math.Sqrt(float64(len(peers)))/10)
 			for _, peer := range peers[:numDirect] {
 				txset[peer] = append(txset[peer], tx.Hash())
 			}
